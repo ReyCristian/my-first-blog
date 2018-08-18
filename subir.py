e303491830cfb0aee123faba741b1562fb684414
Rey.Cristian <Rey.Cristian.eze@gmail.com>
2018-08-18 10:54:18 -0300

    template_base

--- subir.py
+++ subir.py
@@ -7,7 +7,7 @@ commit=input("ingrese el commit:");
 comando ="git commit -m \"" + commit + "\"";
 os.system(comando);
 os.system("git status");
-ok=input("todo esta bien");
+ok=input("todo esta bien?");
 if ok[0]=='s':
 	os.system("git push");
 
